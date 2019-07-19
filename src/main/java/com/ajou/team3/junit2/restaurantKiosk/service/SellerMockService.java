b51bfa529c13d53efe47896788abf80001d41a2a
YongJae <sdc689@gmail.com>
2019-07-19 18:32:25 +0900

    Modify wrong spelling

--- src/main/java/com/ajou/team3/junit2/restaurantKiosk/service/SellerMockService.java
+++ src/main/java/com/ajou/team3/junit2/restaurantKiosk/service/SellerMockService.java
@@ -6,7 +6,7 @@ import com.ajou.team3.junit2.restaurantKiosk.repository.SellerMockRepository;
 import java.util.List;
 
 public class SellerMockService {
-    private final SellerMockRepository sellerMockRepository
+    private final SellerMockRepository sellerMockRepository;
 
     public SellerMockService(SellerMockRepository sellerMockRepository) {
         this.sellerMockRepository = sellerMockRepository;
