d7c23943e29bd0dd8bd21f0565c259c9e067d544
0zoo <e0zoo94@gmail.com>
2019-07-21 05:27:16 +0900

    add customer junit test

--- src/main/java/com/ajou/team3/junit2/restaurantKiosk/repository/CustomerMockRepository.java
+++ src/main/java/com/ajou/team3/junit2/restaurantKiosk/repository/CustomerMockRepository.java
@@ -4,8 +4,7 @@ import com.ajou.team3.junit2.restaurantKiosk.domain.Meal;
 import java.util.List;
 
 public interface CustomerMockRepository {
-    public Meal orderMealByName(String mealName);
-    public void addMealinCart(Meal meal);
-    public int getTotalPrice(List<Meal> meal);
-
+    Meal orderMealByName(String mealName);
+    void addMealinCart(Meal meal);
+    int getTotalPrice(List<Meal> meal);
 }
--- src/main/java/com/ajou/team3/junit2/restaurantKiosk/service/CustomerMockService.java
+++ src/main/java/com/ajou/team3/junit2/restaurantKiosk/service/CustomerMockService.java
@@ -2,14 +2,24 @@ package com.ajou.team3.junit2.restaurantKiosk.service;
 
 import com.ajou.team3.junit2.restaurantKiosk.domain.Meal;
 import com.ajou.team3.junit2.restaurantKiosk.repository.CustomerMockRepository;
+import com.ajou.team3.junit2.restaurantKiosk.repository.MealMockRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerMockService {
     private final CustomerMockRepository customerMockRepository;
+    private final MealMockRepository mealMockRepository;
 
-    public CustomerMockService(CustomerMockRepository customerMockRepository) {
+//    public CustomerMockService(CustomerMockRepository customerMockRepository) {
+//        this.customerMockRepository = customerMockRepository;
+//    }
+    //
+    private List<Meal> customerMealCart = new ArrayList<>();
+
+    public CustomerMockService(CustomerMockRepository customerMockRepository, MealMockRepository mealMockRepository) {
         this.customerMockRepository = customerMockRepository;
+        this.mealMockRepository = mealMockRepository;
     }
 
     public Meal orderMealByName(String mealName) { // lee yong jae
@@ -17,17 +27,51 @@ public class CustomerMockService {
         return meal;
     }
 
-    public void addMealinCart(Meal meal){  //lee yeon ju
+    public void addMealinCart(Meal meal) {  //lee yeon ju
         Meal addMeal = new Meal(meal.getMealName(), meal.getMealPrice(), meal.getMealRemaining());
         customerMockRepository.addMealinCart(addMeal);
         return;
     }
-    public int getTotalPrice(List<Meal> meal){ //lee yeon ju
+
+    public int getTotalPrice(List<Meal> meal) { //lee yeon ju
         int totalPrice = 0;
-        for(int i=0; i<meal.size(); i++){
+        for (int i = 0; i < meal.size(); i++) {
             totalPrice += meal.get(i).getMealPrice();
         }
         return totalPrice;
+    }
+
+    public int addMealToCart(String mealName) {
+        Meal meal = mealMockRepository.getMealByName(mealName);
+
+        if (meal == null) {
+            throw new NullPointerException("존재하지 않는 음식입니다.");
+        } else if (meal.getMealRemaining() == 0) {
+            throw new IllegalArgumentException("재고가 없는 음식입니다.");
+        } else {
+            customerMealCart.add(meal);
+            return getTotalCartPrice();
+        }
+    }
+
+    public int getTotalCartPrice() {
+        int totalPrice = 0;
+        for (Meal meal : customerMealCart) {
+            totalPrice += meal.getMealPrice();
+        }
+        return totalPrice;
+    }
+
+    public void orderMealsInCart() {
+        if (getCustomerCartSize() > 0) {
+            for (Meal meal : customerMealCart) {
+                mealMockRepository.orderMealByName(meal.getMealName());
+            }
+        }
+    }
+
+    public int getCustomerCartSize() {
+        return customerMealCart.size();
     }
 
 }
